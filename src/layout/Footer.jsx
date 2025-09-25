5a6523c517ebe6637a856ba3a5b937d5649688cc
Madu Senff <madu3811@gmail.com>
2025-09-25 20:02:51 -0300

    ajusta className de Footer.jsx

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -7,52 +7,52 @@ import github from "../assets/icons/social-media/github.png";
 const Footer = () => {
   return (
     <footer>
-      <div class="container mt-auto container-footer">
-        <section class="container-footer-top">
-          <div class="div-txt-logo">
+      <div className="container mt-auto container-footer">
+        <section className="container-footer-top">
+          <div className="div-txt-logo">
             <img src="logo_fonte.png" alt="" className="logo-texto" />
           </div>
-          <div class="texts-area row">
-            <div class="col-4 description">
-              <p class="text-light-purple">About Us</p>
-              <span class="text-white">
+          <div className="texts-area row">
+            <div className="col-4 description">
+              <p className="text-light-purple">About Us</p>
+              <span className="text-white">
                 Dice Tales was built by a small team to make life easier for RPG
                 players. If you'd like to help or add a new system, feel free to
                 reach out!
               </span>
             </div>
-            <div class="col mb-continue">
-              <p class="text-ti-purple">Contact Us</p>
-              <div class="ft-box gap-1">
-                <address class="ft-sp-text">-- dicetales&#64;gmail.com</address>
-                <address class="ft-sp-text">-- +55 (42) 9 6969-6969</address>
+            <div className="col mb-continue">
+              <p className="text-ti-purple">Contact Us</p>
+              <div className="ft-box gap-1">
+                <address className="ft-sp-text">-- dicetales&#64;gmail.com</address>
+                <address className="ft-sp-text">-- +55 (42) 9 6969-6969</address>
               </div>
             </div>
-            <div class="col-5 mb-continue">
-              <p class="text-ti-purple">Share your thoughts with us!</p>
-              <div class="input-group" style={{ maxWidth: "100%" }}>
+            <div className="col-5 mb-continue">
+              <p className="text-ti-purple">Share your thoughts with us!</p>
+              <div className="input-group" style={{ maxWidth: "100%" }}>
                 <input
                   type="text"
-                  class="form-control"
+                  className="form-control"
                   placeholder="Your feedback here"
                 />
-                <button class="" type="button">
+                <button className="" type="button">
                   Send
                 </button>
               </div>
             </div>
           </div>
         </section>
-        <div class="purple-line"></div>
-        <section class="icons-area">
-          <div class="imgs">
+        <div className="purple-line"></div>
+        <section className="icons-area">
+          <div className="imgs">
             <img src={facebook} alt="Ícone do Facebook" />
             <img src={instagram} alt="Ícone do Instagram" />
             <img src={twitter} alt="Ícone do Twitter" />
             <img src={github} alt="Ícone do Github" />
           </div>
         </section>
-        <span class="text-ti-purple center-sp">FOLLOW US</span>
+        <span className="text-ti-purple center-sp">FOLLOW US</span>
       </div>
     </footer>
   );
