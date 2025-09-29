bbcd606afc6b1e59f4bbb741d1d4f0c46f07f7fd
Maria Senff <madu3811@gmail.com>
2025-09-28 21:42:55 -0300

    responsividade footer

--- src/features/home/Home.jsx
+++
@@ -1,9 +0,0 @@
-const Home = () => {
-  return (
-    <>
-      <p>Home está funcionando!</p>
-    </>
-  );
-}
-
-export default Home;
--- src/layout/footer/Footer.jsx
+++ src/layout/footer/Footer.jsx
@@ -3,30 +3,38 @@ import { Icon } from "../../shared/icones/Icon";
 
 const Footer = () => {
   return (
-    <footer className="footer">
+    <footer className="footer-layout">
       <div className="container mt-auto container-footer">
         <section className="container-footer-top">
           <div className="div-txt-logo">
             <img src="logo_fonte.png" alt="Logo Dice Tales" className="logo-texto" />
           </div>
           <div className="texts-area row">
-            <div className="col-4 description">
-              <p className="text-light-purple">About Us</p>
+            <div className="col-12 col-md-4 description mb-3 mt-4 mt-md-0 mb-md-0">
+              <p className="text-light-purple text-center text-md-start mb-0 mb-md-2">About Us</p>
               <span className="text-white">
                 Dice Tales was built by a small team to make life easier for RPG
                 players. If you'd like to help or add a new system, feel free to
                 reach out!
               </span>
             </div>
-            <div className="col mb-continue">
-              <p className="text-ti-purple">Contact Us</p>
+
+            <div className="col-12 col-md-3 mb-3 mb-md-0">
+              <p className="text-ti-purple text-center text-md-start mb-0 mb-md-2">Contact Us</p>
               <div className="ft-box gap-1">
-                <address className="ft-sp-text"><Icon name='mail' /> dicetales&#64;gmail.com</address>
-                <address className="ft-sp-text"><Icon name='phone' /> +55 (42) 9 6969-6969</address>
+                <address className="ft-sp-text text-center text-md-start">
+                  <Icon name='mail' className='me-1' /> 
+                  dicetales&#64;gmail.com
+                </address>
+                <address className="ft-sp-text text-center text-md-start">
+                  <Icon name='phone' className='me-1' /> 
+                  +55 (42) 9 6969-6969
+                </address>
               </div>
             </div>
-            <div className="col-5 mb-continue">
-              <p className="text-ti-purple">Share your thoughts with us!</p>
+
+            <div className="col-12 col-md-5 mb-3 mb-md-0">
+              <p className="text-ti-purple text-center text-md-start mb-2 mb-md-2">Share your thoughts with us!</p>
               <div className="input-group" style={{ maxWidth: '100%' }}>
                 <input
                   type="text"
@@ -40,7 +48,7 @@ const Footer = () => {
             </div>
           </div>
         </section>
-        <div className="purple-line"></div>
+        <div className="purple-line d-none d-md-block"></div>
         <section className="icons-area">
           <div className="imgs">
             <Icon name='instagram' size='25' />
