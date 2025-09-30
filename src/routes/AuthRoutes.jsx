9b99ebaf398465a2e863d0626a23c608854a3256
Maria Senff <madu3811@gmail.com>
2025-09-30 20:04:17 -0300

    adiciona página de cadastro

--- src/routes/AuthRoutes.jsx
+++ src/routes/AuthRoutes.jsx
@@ -1,8 +1,10 @@
 import { Route } from 'react-router-dom'
 import Login from '../auth/pages/Login'
+import { Register } from '../auth/pages/Register'
 
 export const AuthRoutes = (
   <Route>
     <Route path='/login' element={<Login />} />
+    <Route path='/register' element={<Register />} />
   </Route>
 ) 
