867e1aba7ce584105b7a298e040bd96012fd7b97
Madu <madu3811@gmail.com>
2025-10-08 20:38:24 -0300

    estiliza sheets

--- src/features/sheets/components/Sheets.jsx
+++ src/features/sheets/components/Sheets.jsx
@@ -1,19 +1,27 @@
+import { useEffect } from 'react';
 import styles from './Sheets.module.scss'
 import classNames from 'classnames/bind';
 
 const cx = classNames.bind(styles)
 
 export const Sheets = ({sheet, userType}) => {
-  if(userType == 'player')
-    console.log(userType) // não sei ainda
-  if(userType == 'gameMaster')
-    console.log(userType)
+  const name_game_sheets = 'Sei lá'
 
   return (
     <div className={cx('sheets-group', 'list-group')}>
-      <a className={cx('sheets', 'list-group-item', 'mb-2')}>
-        {sheet.name_sheet}
-      </a>
+      <div className={cx('sheets', 'list-group-item', 'd-flex flex-row')}>
+        <div className={cx('img-box', 'me-3')}>
+          <img src="" alt="" />
+        </div>
+        <div className={cx('text-box')}>
+          <h6>
+            {sheet.name_sheet}
+          </h6>
+          <p>
+            {name_game_sheets}
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
