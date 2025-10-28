e4934e47ff9e90225fc17429c6512efd443b9ae1
Madu <madu3811@gmail.com>
2025-10-28 20:05:36 -0300

    ajusta rotas no json-server

--- src/App.jsx
+++ src/App.jsx
@@ -6,27 +6,27 @@ import appRoutes from './routes/appRoutes';
 const App = () => {
   const navigate = useNavigate()
   const location = useLocation()
-  const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(false)
 
   const element = useRoutes(appRoutes)
 
-  useEffect(() => {
-    const initAuth = async () => {
-      try {
-        setLoading(true)
-        await refresh()
-      } catch {
-        setLoading(false)
-        if(location.pathname !== '/login' && location.pathname !== '/register') {
-          navigate('/login', {replace: true} )
-        }
-      } finally {
-        setLoading(false)
-      }
-    }
+  // useEffect(() => {
+  //   const initAuth = async () => {
+  //     try {
+  //       setLoading(true)
+  //       await refresh()
+  //     } catch {
+  //       setLoading(false)
+  //       if(location.pathname !== '/login' && location.pathname !== '/register') {
+  //         navigate('/login', {replace: true} )
+  //       }
+  //     } finally {
+  //       setLoading(false)
+  //     }
+  //   }
 
-    initAuth()
-  }, [navigate])
+  //   initAuth()
+  // }, [navigate])
 
   if (loading) {
     return <div>Carregando...</div> // spinner ou algo assim depois faço
