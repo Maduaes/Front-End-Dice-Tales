d10e3e17fdae0bfa937c44ed4c478df449490be4
Guilherme C. Rikaczewski <guilhermecrikaczewski@gmail.com>
2026-02-18 14:39:58 -0300

    adiciona docker compose para dev mode

--- vite.config.js
+++ vite.config.js
@@ -21,10 +21,10 @@ export default defineConfig({
     host: true,
     proxy: {
       "/api": {
-        target: "http://backend:3000",
+        target: "http://localhost:3000",
         changeOrigin: true,
         secure: false,
-        rewrite: (path) => path.replace(/^\/api/, "")
+        // rewrite: (path) => path.replace(/^\/api/, "") 
       },
     },
   }
