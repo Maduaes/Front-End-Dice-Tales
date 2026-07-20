b37daca7c127dfec265f00c7199926cc01a68a92
Guilherme C. Rikaczewski <guilhermecrikaczewski@gmail.com>
2026-07-20 00:25:07 -0300

    inicia funcionamento do botão para criar sheet

--- src/features/sheets/components/CreateSheetModal.jsx
+++ src/features/sheets/components/CreateSheetModal.jsx
@@ -12,15 +12,17 @@ import { Icon } from '../../../shared/icones/Icon'
 //   uploadGameCover,
 // } from '../../../../services/gamesService'
 import { SelectOption } from "@/shared/forms/SelectOption"
-import ais from "./AiSheetModal.module.scss"
+// import ais from "./AiSheetModal.module.scss"
+import { createSheet } from "../../../services/sheetsService"
 
-const gameOptions = [{ id: 1, descricao: "D&D 5e" }]
+const gameOptions = [{ id: 1, descricao: "D&D5e"}, { id: 2, descricao: "T20"}]
 const MAX_FILE_SIZE = 10 * 1024 * 1024
 const ALLOWED_IMAGE_TYPES = ["image/png", "image/jpeg", "image/webp"]
 
 export const CreateSheetModal = ({ type, atualizaGames, dados }) => {
   const [formGame, setFormGame] = useState({ id: null, name: '' })
-  const [formSheet, setSheetName] = useState({ name: '' })
+  const [formSheet, setSheetData] = useState({ name: ''})
+  const [formSystem, setGameData] = useState({ game: 'D&D5e' })
   const [formJoin, setFormJoin] = useState({ code: '' })
   const [coverFile, setCoverFile] = useState(null)
   const [isSubmitting, setIsSubmitting] = useState(false)
@@ -73,7 +75,11 @@ export const CreateSheetModal = ({ type, atualizaGames, dados }) => {
   }
 
   const handleChange = (name, value) => {
-    setSheetName({ [name]: value })
+    setSheetData({ [name]: value })
+  }
+
+  const handleChangeOption = (name, value) => {
+    setGameData({ [name]: value })
   }
 
   const handleCoverChange = (event) => {
@@ -102,7 +108,262 @@ export const CreateSheetModal = ({ type, atualizaGames, dados }) => {
   const handleSubmit = async () => {
     try {
       setIsSubmitting(true)
-      let response = null
+      const defaultDnDSheet = {
+        schema_version: 1,
+
+        char_class: null,
+        level: 1,
+        origin: null,
+        race: null,
+        subrace: null,
+        alignment: null,
+        inspiration: false,
+        proficiency_bonus: 2,
+        armor_class: null,
+        initiative: null,
+        movement_speed: null,
+        total_hp: null,
+        current_hp: null,
+        temp_hp: null,
+        hp_dice_sides: null,
+        total_hp_dices: null,
+        current_hp_dices: null,
+        personality_traits: null,
+        ideals: null,
+        bonds: null,
+        flaws: null,
+        passive_perception: null,
+        passive_perception_mod: 0,
+
+        spell_save_dc: null,
+        spell_attack_bonus: null,
+        spellcasting_attribute: null,
+
+        death_saving_throws: {
+          success: 0,
+          fails: 0
+        },
+
+        attributes: {
+          str: { value: 10, mod: 0 },
+          dex: { value: 10, mod: 0 },
+          con: { value: 10, mod: 0 },
+          int: { value: 10, mod: 0 },
+          wis: { value: 10, mod: 0 },
+          cha: { value: 10, mod: 0 }
+        },
+
+        saving_throws: {
+          str: { default_value: 0, total_value: 0, is_proficient: false },
+          dex: { default_value: 0, total_value: 0, is_proficient: false },
+          con: { default_value: 0, total_value: 0, is_proficient: false },
+          int: { default_value: 0, total_value: 0, is_proficient: false },
+          wis: { default_value: 0, total_value: 0, is_proficient: false },
+          cha: { default_value: 0, total_value: 0, is_proficient: false }
+        },
+
+        inventory: {
+          money: {
+            total_weight: 0,
+            cp: 0,
+            sp: 0,
+            ep: 0,
+            gp: 0,
+            pp: 0
+          },
+          items: []
+        },
+
+        bio: {
+          age: null,
+          size: null,
+          height: null,
+          weight: null,
+          appearance: null,
+          allies: null,
+          backstory: null,
+          treasure: null
+        },
+
+        skills: {
+          acrobatics: {
+            atribute: "dex",
+            proficiency_level: null,
+            jack_of_all_trades_on: false,
+            reliable_talent_on: false,
+            other_mod: 0,
+            total: null
+          },
+          animal_handing: {
+            atribute: "wis",
+            proficiency_level: null,
+            jack_of_all_trades_on: false,
+            reliable_talent_on: false,
+            other_mod: 0,
+            total: null
+          },
+          arcana: {
+            atribute: "int",
+            proficiency_level: null,
+            jack_of_all_trades_on: false,
+            reliable_talent_on: false,
+            other_mod: 0,
+            total: null
+          },
+          athletics: {
+            atribute: "str",
+            proficiency_level: null,
+            jack_of_all_trades_on: false,
+            reliable_talent_on: false,
+            other_mod: 0,
+            total: null
+          },
+          deception: {
+            atribute: "cha",
+            proficiency_level: null,
+            jack_of_all_trades_on: false,
+            reliable_talent_on: false,
+            other_mod: 0,
+            total: null
+          },
+          history: {
+            atribute: "int",
+            proficiency_level: null,
+            jack_of_all_trades_on: false,
+            reliable_talent_on: false,
+            other_mod: 0,
+            total: null
+          },
+          insight: {
+            atribute: "wis",
+            proficiency_level: null,
+            jack_of_all_trades_on: false,
+            reliable_talent_on: false,
+            other_mod: 0,
+            total: null
+          },
+          intimidation: {
+            atribute: "cha",
+            proficiency_level: null,
+            jack_of_all_trades_on: false,
+            reliable_talent_on: false,
+            other_mod: 0,
+            total: null
+          },
+          investigation: {
+            atribute: "int",
+            proficiency_level: null,
+            jack_of_all_trades_on: false,
+            reliable_talent_on: false,
+            other_mod: 0,
+            total: null
+          },
+          medicine: {
+            atribute: "wis",
+            proficiency_level: null,
+            jack_of_all_trades_on: false,
+            reliable_talent_on: false,
+            other_mod: 0,
+            total: null
+          },
+          nature: {
+            atribute: "int",
+            proficiency_level: null,
+            jack_of_all_trades_on: false,
+            reliable_talent_on: false,
+            other_mod: 0,
+            total: null
+          },
+          perception: {
+            atribute: "wis",
+            proficiency_level: null,
+            jack_of_all_trades_on: false,
+            reliable_talent_on: false,
+            other_mod: 0,
+            total: null
+          },
+          performance: {
+            atribute: "cha",
+            proficiency_level: null,
+            jack_of_all_trades_on: false,
+            reliable_talent_on: false,
+            other_mod: 0,
+            total: null
+          },
+          persuasion: {
+            atribute: "cha",
+            proficiency_level: null,
+            jack_of_all_trades_on: false,
+            reliable_talent_on: false,
+            other_mod: 0,
+            total: null
+          },
+          religion: {
+            atribute: "int",
+            proficiency_level: null,
+            jack_of_all_trades_on: false,
+            reliable_talent_on: false,
+            other_mod: 0,
+            total: null
+          },
+          sleight_of_hand: {
+            atribute: "dex",
+            proficiency_level: null,
+            jack_of_all_trades_on: false,
+            reliable_talent_on: false,
+            other_mod: 0,
+            total: null
+          },
+          stealth: {
+            atribute: "dex",
+            proficiency_level: null,
+            jack_of_all_trades_on: false,
+            reliable_talent_on: false,
+            other_mod: 0,
+            total: null
+          },
+          survival: {
+            atribute: "wis",
+            proficiency_level: null,
+            jack_of_all_trades_on: false,
+            reliable_talent_on: false,
+            other_mod: 0,
+            total: null
+          }
+        },
+
+        spell_slots: {
+          level_1: { value: 0, mod: 0 },
+          level_2: { value: 0, mod: 0 },
+          level_3: { value: 0, mod: 0 },
+          level_4: { value: 0, mod: 0 },
+          level_5: { value: 0, mod: 0 },
+          level_6: { value: 0, mod: 0 },
+          level_7: { value: 0, mod: 0 },
+          level_8: { value: 0, mod: 0 },
+          level_9: { value: 0, mod: 0 }
+        },
+
+        customizable_features: [],
+
+        tool_proficiencies: [],
+
+        other_proficiencies: [],
+
+        attacks: [],
+
+        traits: [],
+
+        spells: []
+      }
+      let response = await createSheet(
+        {
+          game_system: formSystem.game,
+          sheet_type: "player",
+          name: formSheet.name,
+          content: defaultDnDSheet,
+        }
+      )
 
       if (isNewGame || isEditGame) {
         response = isNewGame
@@ -154,8 +415,8 @@ export const CreateSheetModal = ({ type, atualizaGames, dados }) => {
             <SelectOption
                 label="Game"
                 name="game"
-                value="teste"
-                handleChange={handleChange}
+                value={formSystem.game}
+                handleChange={handleChangeOption}
                 listaOpcoes={gameOptions}
                 theme="ipt-second"
                 className="col-12"
